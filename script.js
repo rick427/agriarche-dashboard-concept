0d571a8212228c58a9283dba040bdb75e26d1323
rick427 <rchard@yahoo.com>
2021-04-12 20:09:01 +0100

    changed a few things and added price slider

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Bar chart
 const labels = [
-  'Agriarche Ltd',
-  'Alh. Suleiman Yau',
-  'Alh. Abubakar Kabir',
-  'Bashar Sani',
-  'Flo Movina',
-  'Coscharis',
-  'Okwori Mayann',
-  'Animal Care',
-  'Tribe Agric'
+  'Maize',
+  'Soya',
+  'Cassava',
+  'Sorghum',
+  'Rice',
+  'Plantain',
+  'Beans',
+  'Wheat',
+  'Barley'
 ];
 
 const data = {
   labels: labels,
   datasets: [{
-    label: 'Aggregation by Food Processors',
+    label: 'Value of commodity',
     data: [39402160, 6070260, 6699056, 9192880, 33073640, 13249220, 6686400, 1000000, 12000000],
     backgroundColor: ['rgba(244, 148, 37,.2)'],
     borderColor: ['rgba(244, 148, 37, 1)'],
